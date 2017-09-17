cbfbddb88b9d4b9f209347e11e887f9d5216c641
Matthew Ma <justmxk@gmail.com>
2017-09-17 21:08:51 +1000

    Create SignInForm and wrapper in SignInFormContainer

---
+++ src/routes/Home/components/SignInForm.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import {
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+  Row,
+  Col,
+  Button
+} from 'react-bootstrap'
+import {InputField} from '../../../components/InputField'
+import { Field } from 'redux-form'
+import DateTimeField from '../../../components/DateTimeField'
+
+class SignInForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { fields: { firstName, lastName, address, passport, dob, suburb, postcode }, submitting, handleSubmit } = this.props
+    return (
+      <form onSubmit={handleSubmit}>
+        <Row>
+          <Col xs={12}>
+            <Field name="firstName" placeholder='FirstName' lable="First Name" id="firstName" component={InputField} type="text" { ...firstName } />
+          </Col>
+        </Row>
+        <Row>
+        <Col xs={12}>
+          <Field name="lastName" placeholder='LastName' lable="Last Name" id="lastName" component={InputField} type="text" { ...lastName } />
+         </Col>
+        </Row>
+        <Row>
+        <Col xs={12}>
+           <Field name="dob" placeholder='DOB' lable="Date of Birth" id="dob" component={DateTimeField} type="text" { ...dob } />
+         </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Field name="address" placeholder='Address' lable="Address" id="address" component={InputField} type="text" { ...address } />
+         </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Field name="suburb" placeholder='Suburb' lable="Suburb" id="suburb" component={InputField} type="text" { ...suburb } />
+         </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Field name="postcode" placeholder='Postcode' lable="Postcode" id="postcode" component={InputField} type="text" { ...postcode } />
+         </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Field name="passport" placeholder='Passport' lable="Passport" id="passport" component={InputField} type="text" { ...passport } />
+         </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Button type="submit" disabled={submitting} className="btn-md right">
+              { submitting ? <i className="fa fa-refresh fa-spin fa-2x "></i> : "Submit" }
+            </Button>
+          </Col>
+        </Row>
+      </form>
+    )
+  }
+}
+
+export default SignInForm
---
+++ src/routes/Home/containers/SignInFormContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import SignInForm from '../components/SignInForm'
+import { reduxForm } from 'redux-form'
+import { createValidator, required, postcode } from '../../../utils/form_validation'
+import memoize from 'lru-memoize'
+
+const formValidation = createValidator({
+  firstName: required,
+  lastName: required,
+  address: required,
+  dob: required,
+  passport: required,
+  suburb: required,
+  postcode: [required, postcode],
+});
+const validation = memoize(10)(formValidation);
+
+const SignInFormContainer = reduxForm({
+  form: 'singIn',
+  validate: validation,
+  fields: ['firstName', 'lastName', 'address', 'dob', 'passport', 'suburb', 'postcode']
+})(SignInForm)
+
+export default SignInFormContainer
