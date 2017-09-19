11451e761279cc32c63ca5dccef1396b4c6dd054
Matthew Ma <justmxk@gmail.com>
2017-09-19 13:24:47 +1000

    Setup jest and test Document page

---
+++ src/routes/Document/components/__test__/Document.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Document from '../Document'
+import { userDetails, files } from '../../../../utils/mockData'
+
+const router = {
+  push: jest.fn()
+}
+
+describe('<Document />', () => {
+  it('renders a Document using Enzyme', () => {
+    const wrapper = shallow(
+      <Document
+        user={userDetails}
+        router={router}
+      />
+    )
+    expect(wrapper.find('#user_detail').length).toBe(1)
+  })
+
+  it('will show error message if no files loaded and submit button is clicked', () => {
+    const wrapper = shallow(
+      <Document
+        user={userDetails}
+        router={router}
+      />
+    )
+    wrapper.find('#submit_button').simulate('click')
+    expect(wrapper.find('.error_msg').length).toBe(1)
+    expect(wrapper.contains(
+      <h6 className='error_msg'>
+        You have to submit Mandatory files and Supporting files to continue
+      </h6>
+      ))
+  })
+  it('router.push will be called if both mandatory and support files are loaded', () => {
+    const wrapper = shallow(
+      <Document
+        user={userDetails}
+        router={router}
+      />
+    )
+    wrapper.setState({ acceptedFiles: files })
+    const routerProp = wrapper.instance().props.router
+    const routeTo = jest.spyOn(routerProp, 'push')
+    wrapper.find('#submit_button').simulate('click')
+    expect(routeTo).toHaveBeenCalled()
+  })
+})
---
+++ src/utils/mockData.js
@@ -0,0 +1,20 @@
+export const userDetails = {
+  firstName: 'John',
+  lastName: 'Lee',
+  address: '24 Market St',
+  suburb: 'Sydney',
+  postcode: '2000',
+  dob: '1990-09-17',
+  passport: 'Q1234566778'
+}
+
+export const files = [
+  {
+    file: { name: 'passport.pdf' },
+    type: 'mandatory'
+  },
+  {
+    file: { name: 'retal_scan.png' },
+    type: 'support'
+  }
+]
